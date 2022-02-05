8b6c20d4218bee327c73586f7b60caf0305d82f4
Joe Simopoulos <joesimop8@gmail.com>
2022-02-04 17:55:56 -0800

    Adds Bootstrap and SCSS, edits some files to ensure everything's working together.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
-import './App.css';
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styling/App.scss';
 
 function App() {
   return (
@@ -8,7 +10,16 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+         </p>
+         <Card style={{ width: '18rem', color: 'black' }}>
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                </Card.Text>
+            </Card.Body>
+        </Card>
         <a
           className="App-link"
           href="https://reactjs.org"
