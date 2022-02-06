ccb75cb62a67263221a17d8c50b783308aac3463
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2022-02-06 12:23:36 -0800

    Added grid breakpoints for [1, 4] columns

--- frontend/src/components/UserPage.js
+++ frontend/src/components/UserPage.js
@@ -2,13 +2,12 @@ import Tile from './tiles/Tile'
 import {Container, Row, Col} from 'react-bootstrap'
 
 function UserPage(props) {
-    const columnConversion = 3;
     return (
         <Container fluid="true" className="p-3">
             <Row className="g-3">
                 {props.tiles.map((tile, index) => { 
                     return (
-                        <Col className={`tile-index-${index}`} xs={{span: tile.width * columnConversion}}>{ Tile(tile) }</Col>
+                        <Col className={`tile-index-${index}`} xs={12} sm={tile.width * 6} md={tile.width * 4} lg={tile.width * 3}>{ Tile(tile) }</Col>
                     );
                 })}
             </Row>
