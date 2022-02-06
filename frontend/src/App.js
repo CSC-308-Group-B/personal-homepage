d54acbb3ebb0155f03068c63a91a0c51c3b1f7fa
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2022-02-05 20:06:06 -0800

    added basic tile rendering method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
-import Card from 'react-bootstrap/Card';
+// import Card from 'react-bootstrap/Card';
+import Tile from './Tile'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styling/App.scss';
 
 function App() {
+  //We can use a static sample user, for now, and adjust the object scheme. Later on we will implement this on the backend/database
+  const user = {
+    name: "Gertrude",
+    id: "123abc",
+    tiles: [
+      {
+        tileType: "blank",
+        width: 2,
+        height: 4,
+      },
+      {
+        tileType: "blank",
+        width: 1,
+        height: 2,
+      },
+      {
+        tileType: "blank",
+        width: 1,
+        height: 2,
+      },
+      {
+        tileType: "blank",
+        width: 2,
+        height: 3,
+      },
+      {
+        tileType: "blank",
+        width: 2,
+        height: 1,
+      },
+      {
+        tileType: "blank",
+        width: 3,
+        height: 1,
+      },
+    ]
+  };
+
+  //if the user and their tile list is defined, render each tile
+  let content = "";
+  if (user && user.tiles) {
+    content = user.tiles.map(function(tile){
+      return Tile(tile);
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-         </p>
-         <Card style={{ width: '18rem', color: 'black' }}>
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                </Card.Text>
-            </Card.Body>
-        </Card>
+        </p>
+        <div style={{"display": "grid", "grid-template-columns": "1fr 1fr 1fr 1fr"}}>
+          { content }
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
