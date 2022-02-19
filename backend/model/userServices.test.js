376baf316f21a99aabaa5d38bbcb911a97620d58
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2022-02-18 16:05:01 -0800

    started adding tests

---
+++ backend/model/userServices.test.js
@@ -0,0 +1,216 @@
+const mongoose = require("mongoose");
+const UserSchema = require("./userSchema");
+const userServices = require("./userServices");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const { add } = require("./tileSchema");
+
+let mongoServer;
+let conn;
+let userModel;
+
+beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+    const mongooseOpts = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    };
+    conn = await mongoose.createConnection(uri, mongooseOpts);
+    userModel = conn.model("User", UserSchema);
+    userServices.setDbConnection(conn);
+});
+
+afterAll(async () => {
+    await conn.dropDatabase();
+    await conn.close();
+    await mongoServer.stop();
+});
+
+beforeEach(async () => {
+    //first dummyUser
+    let dummyUser = {
+        name: "Nicol Bolas",
+        email: "dragon@gmail.com",
+        tiles: [
+            {
+                tileType: "noTileType",
+                width: 2,
+                x: 0,
+                y: 200,
+            },
+            {
+                tileType: "noTileType",
+                width: 2,
+                x: 25,
+                y: 400,
+            },
+            {
+                tileType: "noTileType",
+                width: 2,
+                x: 75,
+                y: 600,
+            },
+        ]
+    }
+    let result = new userModel(dummyUser);
+    await result.save();
+    //second dummyUser
+    dummyUser = {
+        name: "Jace, the Mind Sculptor",
+        email: "iforgot@gmail.com",
+        tiles: [
+            {
+                tileType: "noTileType",
+                width: 2,
+                x: 0,
+                y: 200,
+            },
+            {
+                tileType: "noTileType",
+                width: 2,
+                x: 25,
+                y: 400,
+            },
+            {
+                tileType: "noTileType",
+                width: 2,
+                x: 75,
+                y: 600,
+            },
+        ]
+    }
+    result = new userModel(dummyUser);
+    await result.save();
+    //third dummyUser
+    dummyUser = {
+        name: "Ragavan",
+        email: "monke@gmail.com",
+        tiles: [
+            {
+                tileType: "noTileType",
+                width: 2,
+                x: 0,
+                y: 200,
+            },
+            {
+                tileType: "noTileType",
+                width: 2,
+                x: 25,
+                y: 400,
+            },
+            {
+                tileType: "noTileType",
+                width: 2,
+                x: 75,
+                y: 600,
+            },
+        ]
+    }
+    result = new userModel(dummyUser);
+    await result.save();
+});
+
+afterEach(async () => {
+    await userModel.deleteMany();
+});
+
+
+//TEST CASES:
+
+
+//getUsers()
+test("Fetching all users", async () => {
+    const users = await userServices.getUsers();
+    expect(users).toBeDefined();
+    expect(users.length).toBeGreaterThan(0);
+    expect(users.length).toBe(3);
+});
+
+//getUserById()
+test("Fetching user by id - doesn't exist", async () => {
+    const foundUser = await userServices.getUserById("notAnId");
+    expect(foundUser).toBeUndefined();
+});
+test("Fetching user by id - newly added", async () => {
+    const dummyUser = {
+        name: "Fblthp",
+        email: "fblthp@gmail.com",
+        tiles: [
+            {
+                tileType: "noTileType",
+                width: 2,
+                x: 0,
+                y: 200,
+            },
+        ]
+    }
+    const result = new userModel(dummyUser);
+    const addedUser = await result.save();
+    const foundUser = await userServices.getUserById(addedUser.id);
+    expect(foundUser).toBeDefined();
+    expect(foundUser.name).toBe(addedUser.name);
+    expect(foundUser.email).toBe(addedUser.email);
+    expect(foundUser.tiles.length).toBe(addedUser.tiles.length);
+});
+
+//getUserByEmail()
+test("Fetch user by email - doesn't exist", async () => {
+    const foundUser = await userServices.getUserByEmail("fblthp@gmail.com");
+    expect(foundUser).toBeUndefined();
+});
+test("Fetch user by email - exists", async () => {
+    const foundUser = await userServices.getUserByEmail("monke@gmail.com");
+    expect(foundUser).toBeDefined();
+    expect(foundUser.name).toBe("Ragavan");
+});
+
+//addUser()
+test("add user - invalid", async () => {
+    const dummyUser = {
+        namasdfasdfe: "Fblthp",
+        emasdfasdfasdail: "fblthp@gmail.com",
+        tifasdfasdfasdfasdfasdfles: [
+            {
+                tileType: "noTileType",
+                width: 2,
+                x: 0,
+                y: 200,
+            },
+        ]
+    }
+    const addedUser = await userServices.addUser(dummyUser);
+    expect(addedUser).toBe(false);
+});
+test("add user", async () => {
+    const dummyUser = {
+        name: "Fblthp",
+        email: "fblthp@gmail.com",
+        tiles: [
+            {
+                tileType: "noTileType",
+                width: 2,
+                x: 0,
+                y: 200,
+            },
+        ]
+    }
+    const addedUser = await userServices.addUser(dummyUser);
+    expect(addedUser).toBeDefined();
+    expect(addedUser.name).toBe("Fblthp");
+    expect(addedUser.email).toBe("fblthp@gmail.com");
+    expect(addedUser.tiles.length).toBe(1);
+});
+
+//deleteUserById()
+test("Delete user by id - doesn't exist", async () => {
+    const deletedUser = await userServices.deleteUserById("61fdb8eca419f17fb12d92c4");
+    expect(deletedUser).toBeNull();
+});
+test("Delete user by id - newly added", async () => {
+    const foundUser = await userServices.getUserByEmail("monke@gmail.com");
+    const deletedUser = await userServices.deleteUserById(foundUser._id);
+    expect(deletedUser).toBeDefined();
+    expect(deletedUser.name).toBe(foundUser.name);
+    expect(deletedUser.email).toBe(foundUser.email);
+    expect(deletedUser.tiles.length).toBe(foundUser.tiles.length);
+});
