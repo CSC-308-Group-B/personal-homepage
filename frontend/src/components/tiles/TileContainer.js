11dfdd2a90635b51c7c6faf52fdcab85e1d2ddf3
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2022-04-11 15:44:42 -0700

    frontend

--- frontend/src/components/tiles/TileContainer.js
+++ frontend/src/components/tiles/TileContainer.js
@@ -40,6 +40,19 @@ class Tile extends React.Component {
         }   
     }
 
+    moveTop = async () => {
+        console.log("top!")
+    }
+    moveUp = async () => {
+        console.log("up!")
+    }
+    moveDown = async () => {
+        console.log("down!")
+    }
+    moveBottom = async () => {
+        console.log("bottom!")
+    }
+
     render() {
         //Translates the tile to the coordinates specified in the x and y properties of the tile.
         let transform = {
@@ -55,24 +68,29 @@ class Tile extends React.Component {
         return (
             //These data parameters are so interact.js knows the initial position of the tiles.
             <div
-                className={`TileContainer ${this.props.canEdit ? "draggable" : ""}`}
+                className={`TileContainer ${this.props.canEdit ? "draggable Editing" : ""}`}
                 id={this.props._id}
                 style={transform}
                 data-x={this.props.x}
                 data-y={this.props.y}
-                data-snaptogrid={this.props.snapToGrid}
-            >
+                data-snaptogrid={this.props.snapToGrid} >
 
                 {getTileType(this.props)}
 
                 {this.props.canEdit &&
                     <div className="TileControls">
                         <DropdownButton className="TileEditButton" title="" >
-                            <DropdownButton title="Width">
+                            <DropdownButton title="Width" className="TileEditWidth">
                                 <Dropdown.Item onClick={() => this.setWidth(1)}>Small</Dropdown.Item>
                                 <Dropdown.Item onClick={() => this.setWidth(2)}>Medium</Dropdown.Item>
                                 <Dropdown.Item onClick={() => this.setWidth(3)}>Large</Dropdown.Item>
                                 <Dropdown.Item onClick={() => this.setWidth(4)}>Full</Dropdown.Item>
+                            </DropdownButton>
+                            <DropdownButton title="Reorder" className="TileEditOrder">
+                                <Dropdown.Item onClick={() => this.moveTop(1)}>Move To Top</Dropdown.Item>
+                                <Dropdown.Item onClick={() => this.moveUp(2)}>Move Up</Dropdown.Item>
+                                <Dropdown.Item onClick={() => this.moveDown(3)}>Move Down</Dropdown.Item>
+                                <Dropdown.Item onClick={() => this.moveBottom(4)}>Move To Bottom</Dropdown.Item>
                             </DropdownButton>
                             <Dropdown.Divider ></Dropdown.Divider>
                             <Dropdown.Item onClick={() => this.props.deleteTile(this.props._id)}>Delete</Dropdown.Item>
