4587ff62ba4a080c88f8aadb11bdec79891b172d
GelzoneXUnsas <51806064+GelzoneXUnsas@users.noreply.github.com>
2022-04-12 00:47:07 -0700

    update toggle mode UI

--- frontend/src/components/EditHeader.js
+++ frontend/src/components/EditHeader.js
@@ -1,13 +1,5 @@
 import React from 'react';
-import Dropdown from 'react-bootstrap/Dropdown'
-import DropdownButton from 'react-bootstrap/DropdownButton'
-import FormCheck from 'react-bootstrap/FormCheck'
-import InputGroup from "react-bootstrap/InputGroup";
 import { HexColorPicker } from "react-colorful";
-import Tab from 'react-bootstrap/Tab'
-import Tabs from 'react-bootstrap/Tabs'
-import Button from 'react-bootstrap/Button'
-import BootstrapSwitchButton from 'bootstrap-switch-button-react'
 
 
 class EditHeader extends React.Component {
@@ -41,35 +33,7 @@ class EditHeader extends React.Component {
         }
 
         return (
-            // <div className={classes}>
 
-            //     <Tabs defaultActiveKey="tiles" id="uncontrolled-tab-example" className="mb-3">
-            //         <Tab eventKey="tiles" title="Tiles">
-            //             <div>
-            //                 <p className = 'd-inline-block'>Grid Snapping&nbsp;</p>                   
-            //                 <BootstrapSwitchButton className="ml-4" checked={true} onChange={() => this.props.toggleSnap()} size = 'sm' />
-            //             </div>
-            //             <Dropdown.Item onClick={() => this.props.addTile("ToDoListTile")}>Todo List</Dropdown.Item>
-            //             <Dropdown.Item onClick={() => this.props.addTile("BookmarksTile", { width: 2 })}>Bookmarks</Dropdown.Item>
-            //             <Dropdown.Item onClick={() => this.props.addTile("SearchBarTile", { width: 2 })}>Search Bar</Dropdown.Item>
-            //             <Dropdown.Item onClick={() => this.props.addTile("GradesTile")}>Grades</Dropdown.Item>
-            //             <Dropdown.Item onClick={() => this.props.addTile("UpcomingAssignmentsTile")}>Assignments</Dropdown.Item>
-            //             <Dropdown.Item onClick={() => this.props.addTile("OtherTileString")}>(Other type)</Dropdown.Item>
-            //         </Tab>
-            //         <Tab eventKey="color" title="Color">
-            //             <HexColorPicker className="my-2" color={this.state.color} onChange={this.updateColor} />
-
-            //             <div className="colorSwab my-2" style={{ borderColor: this.state.color }}>
-            //                 Color: {this.state.color}
-            //             </div>
-
-            //             <InputGroup>
-            //                 <input id="inputBackgroundImageURL" value={this.props.backgroundImage} onChange={() => this.updateBackground(this.value)} />
-            //                 <button onClick={() => { document.getElementById("inputBackgroundImageURL").value = ""; this.updateBackground("") }}>Reset</button>
-            //             </InputGroup>
-            //         </Tab>
-            //     </Tabs>
-            // </div >
             <div>
                   <div className={classname}>
                     <div class="dropdown" className="headers">
@@ -92,6 +56,13 @@ class EditHeader extends React.Component {
                           <button onClick={() => { document.getElementById("inputBackgroundImageURL").value = ""; this.updateBackground("") }}>Reset</button>
                         </div>
                       </div>
+                    </div>
+                    <div class="dropdown" className="headers">
+                    <a className="dropbtn" href="/#">TOGGLE</a>
+                      <label class="switch">
+                        <input type="checkbox" onChange={() => this.props.toggleSnap()}/>
+                        <span class="slider round"></span>
+                      </label>
                     </div>
                   </div>           
             </div>
