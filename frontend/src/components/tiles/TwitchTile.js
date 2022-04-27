88a8f380ffa1f74c9ea350adadaa9126964f5edf
Taylor1818 <tbcmorgan@gmail.com>
2022-04-27 13:28:10 -0700

    aspect ratio fixed

--- frontend/src/components/tiles/TwitchTile.js
+++ frontend/src/components/tiles/TwitchTile.js
@@ -37,8 +37,6 @@ class TwitchTile extends React.Component {
                     <ReactPlayer
                         className="TwitchPlayer"
                         url={`https://www.twitch.tv/${this.state.streamer}`}
-                        width="100%"
-                        height="100%"
                     />
                 </Card.Body>
             </Card>
