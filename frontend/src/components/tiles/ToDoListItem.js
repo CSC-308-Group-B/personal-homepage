3fbd8ae07675f2340c4e2993b601ab3c022cff46
GelzoneXUnsas <51806064+GelzoneXUnsas@users.noreply.github.com>
2022-04-26 19:15:43 -0700

    update on working strike through

--- frontend/src/components/tiles/ToDoListItem.js
+++ frontend/src/components/tiles/ToDoListItem.js
@@ -5,19 +5,29 @@ import React from "react";
 class ToDoListItem extends React.Component {
     itemChecked = (event) => {
         if (event.target.checked) {
-            this.props.updateTask(this.props._id);
-        } else {
             this.props.deleteTask(this.props._id);
         }
     };
 
+    itemClicked = (event) => {
+        if (!event.target.checked){
+            if (this.props.status === 0)
+                this.props.finishTask(this.props._id);
+            if (this.props.status === 1)
+                this.props.redoTask(this.props._id);
+        }
+    };
+
     render() {
+
+        let taskClassName = "task";
+        if (this.props.status === 1) taskClassName += " completed";
+
         return (
-            <ListGroup.Item className="task">
+            <ListGroup.Item className={taskClassName} onClick={this.itemClicked}>
                 {this.props.text}
                 <FormCheck
                     onChange={this.itemChecked}
-                    defaultChecked={this.props.status}
                     className="checkbox"
                 />
             </ListGroup.Item>
--- frontend/src/components/tiles/ToDoListTile.js
+++ frontend/src/components/tiles/ToDoListTile.js
@@ -86,7 +86,7 @@ class ToDoListTile extends React.Component {
         }
     };
 
-    updateTask = async (itemId) => {
+    finishTask = async (itemId) => {
         //send the delete request
         const response = await axios.post(
             `${backendURL}/updateToDoItem`,
@@ -103,6 +103,31 @@ class ToDoListTile extends React.Component {
             //for a valid response, update the item and update our state
             for (let task of this.state.tasks) {
                 if (task._id === itemId) task.status = 1;
+            }
+            await this.setState({ tasks: this.state.tasks });
+        } else {
+            //otherwise, log the error to the console
+            console.log("Failed to update task.");
+        }
+    };
+
+    redoTask = async (itemId) => {
+        //send the delete request
+        const response = await axios.post(
+            `${backendURL}/updateToDoItem`,
+            {
+                userId: this.props.userId,
+                tileId: this.props._id,
+                itemId: itemId,
+                status: 0,
+            },
+            { withCredentials: true }
+        );
+        //if we get a response...
+        if (response && response.status === 200) {
+            //for a valid response, update the item and update our state
+            for (let task of this.state.tasks) {
+                if (task._id === itemId) task.status = 0;
             }
             await this.setState({ tasks: this.state.tasks });
         } else {
@@ -132,7 +157,8 @@ class ToDoListTile extends React.Component {
                                         key={task._id}
                                         {...task}
                                         deleteTask={this.deleteTask}
-                                        updateTask={this.updateTask}
+                                        finishTask={this.finishTask}
+                                        redoTask={this.redoTask}
                                     />
                                 );
                             })}
