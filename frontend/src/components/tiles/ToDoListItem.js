c2881aebfab34e7c7d79441a768fe32faa155600
GelzoneXUnsas <51806064+GelzoneXUnsas@users.noreply.github.com>
2022-04-27 15:53:56 -0700

    update on styling todolist items

--- frontend/src/components/tiles/ToDoListItem.js
+++ frontend/src/components/tiles/ToDoListItem.js
@@ -1,19 +1,18 @@
 import ListGroup from "react-bootstrap/ListGroup";
 import FormCheck from "react-bootstrap/FormCheck";
 import React from "react";
+import CloseButton from "react-bootstrap/esm/CloseButton";
 
 class ToDoListItem extends React.Component {
-    itemChecked = (event) => {
-        if (event.target.checked) {
+    itemClosed = (event) => {
+        if (event.target.className.includes("ToDoListCloseButton"))
             this.props.deleteTask(this.props._id);
-        }
     };
 
     itemClicked = (event) => {
-        if (!event.target.checked) {
-            if (this.props.status === 0) this.props.finishTask(this.props._id);
-            if (this.props.status === 1) this.props.redoTask(this.props._id);
-        }
+        if (event.target.className.includes("ToDoListCloseButton")) return;
+        if (this.props.status === 0) this.props.finishTask(this.props._id);
+        if (this.props.status === 1) this.props.redoTask(this.props._id);
     };
 
     render() {
@@ -26,7 +25,11 @@ class ToDoListItem extends React.Component {
                 onClick={this.itemClicked}
             >
                 {this.props.text}
-                <FormCheck onChange={this.itemChecked} className="checkbox" />
+                <CloseButton
+                    className="ToDoListCloseButton"
+                    onClick={this.itemClosed}
+                ></CloseButton>
+                {/* <FormCheck onChange={this.itemChecked} className="checkbox" /> */}
             </ListGroup.Item>
         );
     }
