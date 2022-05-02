763d05572c7b106a9722d2f8aefa239f5c354691
Taylor1818 <tbcmorgan@gmail.com>
2022-05-02 16:48:45 -0700

    just import

--- frontend/src/components/tiles/TwitchTile.js
+++ frontend/src/components/tiles/TwitchTile.js
@@ -2,13 +2,11 @@ import Card from "react-bootstrap/Card";
 import React from "react";
 import ReactPlayer from "react-player";
 
-const EMBED_URL = "https://embed.twitch.tv/embed/v1.js";
-
 class TwitchTile extends React.Component {
     constructor() {
         super();
         this.state = {
-            streamer: "magic",
+            streamer: "",
         };
     }
 
@@ -19,42 +17,9 @@ class TwitchTile extends React.Component {
 
     handleKeyPress = (event) => {
         if (event.key === "Enter") {
-            this.updateStreamer(this.value);
-            this.generatePlayer(this.state.streamer);
+            this.updateStreamer();
         }
     };
-
-    generatePlayer(streamer) {
-        let embed = window.Twitch.Embed(this.props.targetID, {
-            width: "100%",
-            height: "100%",
-            channel: streamer,
-            layout: "video",
-            autoplay: true,
-            parent: ["localhost"],
-        });
-        var player = embed.getPlayer();
-        player.play();
-    }
-
-    componentDidMount() {
-        let embed;
-        const script = document.createElement("script");
-        script.setAttribute("src", EMBED_URL);
-        script.addEventListener("load", () => {
-            embed = new window.Twitch.Embed(this.props.targetID, {
-                width: "100%",
-                height: "100%",
-                channel: this.state.streamer,
-                layout: "video",
-                autoplay: true,
-                parent: ["localhost"],
-            });
-        });
-        var player = embed.getPlayer();
-        player.play();
-        document.body.appendChild(script);
-    }
 
     render() {
         return (
@@ -70,30 +35,17 @@ class TwitchTile extends React.Component {
                         />
                     </Card.Title>
 
-                    <div>
-                        <div
-                            className="TwitchPlayer"
-                            id={this.props.targetID}
-                        ></div>
-                    </div>
-
-                    {/* <ReactPlayer
+                    <ReactPlayer
                         className="TwitchPlayer"
                         url={`https://www.twitch.tv/${this.state.streamer}`}
                         width="100%"
                         height="100%"
-                    /> */}
+                        playing = {true}
+                    />
                 </Card.Body>
             </Card>
         );
     }
 }
-
-TwitchTile.defaultProps = {
-    targetID: "twitch-embed",
-    width: "940",
-    height: "480",
-    channel: "",
-};
 
 export default TwitchTile;
