af17fad1c27b8e332eaefef754a5cb34e0c55175
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2022-05-28 16:20:06 -0700

    npm run test ignores cypress

---
+++ backend/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    //Comment out this line to include cypress in npm run test calls
+    //Doing so *will* include cypress in CI/CD, however
+    testPathIgnorePatterns: ["backend/cypress/"],
+ }
