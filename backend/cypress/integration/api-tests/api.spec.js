79b161e403762a2d19ce840c2f898f1df6cdb755
Joe Simopoulos <joesimop8@gmail.com>
2022-06-02 15:06:00 -0700

    temp

--- backend/cypress/integration/api-tests/api.spec.js
+++ backend/cypress/integration/api-tests/api.spec.js
@@ -1,18 +1,94 @@
 /// <reference types="cypress" />
 
-describe('first api test', () => {
-  before = () => {
-    `${Cypress.env().BE_URL}/getUser`
-  }
-  context('getting a user', () => {
-    it('GIVEN the backend is running', () => {})
-    
-    it('WHEN I visit the getUser endpoint', () => {
-      cy.request(`${Cypress.env().BE_URL}/getUser`).then((response) => {
-        assert.isNotNull(response.body, 'THEN it gets a response');
-        assert.equal(response.body.name, 'Baba Shaniqua', 'AND returns a user');
-        assert.equal(response.status, 200, 'AND the response code is 200');
-    });
+let testUser, apiResponse;
+
+describe('API Tests', () => {
+    context('Getting a User', () => {
+        it('GIVEN the backend is running', () => { })
+
+        it('WHEN I visit the getUser endpoint', () => {
+            cy.request(`${Cypress.env().BE_URL}/getUser`).then((response) => {
+                apiResponse = response;
+            });
+        })
+
+        it('THEN the repsonse is not null', () => {
+            assert.isNotNull(apiResponse.body, 'THEN it gets a response');
+        })
+
+        it('AND returns a user', () => {
+            assert.equal(apiResponse.body.name, 'Baba Shaniqua', 'AND returns a user');
+        })
+
+        it('AND the reponse code is 200', () => {
+            assert.equal(apiResponse.status, 200, 'AND the response code is 200');
+        })
     })
-  })
+
+    context('Adding a tile to User\'s Tiles', () => {
+        it('GIVEN the backend is running', () => { })
+        it('GIVEN the user is valid', () => { })
+        it('WHEN send a POST request to add a tile', () => {
+            cy.request("POST", `${Cypress.env().BE_URL}/u/111/tiles`, { tileType: "SearchBarTile", width: 2, x: 50, y: 8, color: { r: 100, b: 120, g: 140, a: 0.81 } }).then((response) => {
+                apiResponse = response;
+            });
+        })
+
+        it('THEN the repsonse is not null', () => {
+            assert.isNotNull(apiResponse.body, 'THEN it gets a response');
+        })
+
+        it('AND the response code is 201 (object was created)', () => {
+            assert.equal(apiResponse.status, 201, 'AND the response code is 201, object was created');
+        })
+    })
+
+})
+
+describe('E2E Tests', () => {
+    before(() => {
+        it('GIVEN the user is valid', () => {
+            cy.request(`${Cypress.env().BE_URL}/getUser`).then((response) => {
+                assert.isNotNull(apiResponse.body, 'valid repsonse');
+                testUser = response.body;
+            })
+        })
+        cy.visit(`${Cypress.env().FE_URL}`);
+    });
+
+    context('Removing a tile to User\'s Tiles', () => {
+
+
+        it('GIVEN a user is in Editmode', () => {
+            cy.get('.EditModeToggler').click()
+            cy.get('.canEdit').should('exist')
+        });
+
+        it('WHEN they open a tile dropdown AND click \'Delete\'', () => {
+            cy.get('.TileControls').invoke('show').click({ multiple: true }).get('.show > .Danger').click();
+        })
+
+        it('THEN assert no tiles exist on the page', () => {
+            cy.get('.TileControls').should('not.exist');
+        })
+    })
+
+    context('Logging out of account', () => {
+
+        it('GIVEN a user is in EditMode', () => {
+            cy.get('.canEdit').should('exist')
+        });
+
+        it('AND they click the logout button', () => {
+            cy.get('.logout').click();
+        })
+
+        it('THEN there should be no user when called', () => {
+            cy.request(`${Cypress.env().BE_URL}/getUser`).then((response) => {
+                cy.log(response.body)
+                assert.isNull(response.body, 'valid repsonse');
+            })
+        })
+    })
+
 })
